ad4c03d2ce9d94e254c1f1620a276d44f800750a
Randun Gayantha <randun.gayantha@gmail.com>
2026-02-16 19:23:34 +0530

    Designed Bottom Navigation Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:plantdoc/pages/camera.dart';
+import 'package:plantdoc/pages/fourm.dart';
 import 'package:plantdoc/pages/home.dart';
+import 'package:plantdoc/pages/profile.dart';
+import 'package:plantdoc/pages/map.dart';
 
 void main(){
   runApp(myApp());
@@ -13,11 +17,65 @@ class myApp extends StatefulWidget {
 }
 
 class _myAppState extends State<myApp> {
+
+  int _currentIndex = 0 ;
+
+  final List<Widget> _pages = [
+    Home(),
+    Map(),
+    Camera(),
+    fourm(),
+    Profile()
+
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Plant Doc",
-      home: Home(),
+      home: Scaffold(
+        bottomNavigationBar: BottomNavigationBar( 
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.grey,
+          currentIndex: _currentIndex,
+          onTap: (index){
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Dashbord",
+              ),
+
+            BottomNavigationBarItem(
+              icon: Icon(Icons.map),
+              label: "Map",
+              ),
+
+            BottomNavigationBarItem(
+              icon: Icon(Icons.camera),
+              label: "Camera",
+              ),
+
+            BottomNavigationBarItem(
+              icon: Icon(Icons.forum),
+              label: "Forum",
+              ),
+
+            BottomNavigationBarItem(
+              icon: Icon(Icons.man_2),
+              label: "Profile",
+              ),
+          ],
+
+        ),
+
+        body: _pages[_currentIndex],
+      ),
     );
   }
 }
